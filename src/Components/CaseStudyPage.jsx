42ee83a6cb9dafc0c0dd3c6c93d07319c16f2c93
DingoDev <randilea@outlook.com>
2025-12-05 17:55:34 -0700

    Revised case study page

--- src/Components/CaseStudyPage.jsx
+++ src/Components/CaseStudyPage.jsx
@@ -3,21 +3,19 @@ function CaseStudyPage() {
     <div>
       <h2 className="info-h">Learn more about this project!</h2>
       <p className="info-p">
-        This is a React application designed to showcase a collection of authors
-        and their works. Built with ReactJS, the app presents an interactive
-        grid of books with a filtering system that allows users to browse by
-        author. It’s a project that blends clean design with structured data,
-        making it both a visually engaging and technically sound example of my
-        development skills.
+        This is a React site I designed to showcase a collection of authors and
+        their books. Built with ReactJS, it uses an interactive grid of books
+        with a filtering system (through dropdow) that allows users to browse by
+        author.
       </p>
       <p className="info-p">
-        The application currently uses structured arrays to manage content such
-        as author bios, book details, cover images, and external purchase links.
-        These data sets are mapped into reusable components that dynamically
-        generate dropdown menus, book grids, and branches off into individual
-        author bio pages. This approach not only demonstrates my ability to
-        design scalable front-end architecture, but also prepares the app for
-        future integration with a backend database like MongoDB.
+        The application currently uses arrays to manage content, such as author
+        bios, book details, cover images, and external purchase links. These
+        data sets are mapped into components that dynamically generate dropdown
+        menus, book grids, and also branches off into individual author bio
+        pages. This approach not only demonstrates my ability to design scalable
+        front-end architecture but also prepares the app for future integration
+        with a backend database like MongoDB.
       </p>
       <p className="info-p">
         The next phase will involve migrating the data to that MongoDB backend,
